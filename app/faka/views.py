54a7c4403abdc679e0920b0c89917fa26ff5dc31
tangrela <tumbzzc@qq.com>
2018-09-19 16:31:59 +0800

    让有赞回调吧

--- app/faka/views.py
+++ app/faka/views.py
@@ -6,7 +6,21 @@ from . import faka
 from .. import db
 from ..models import *
 import requests
-from ..youzan import getqr,query_trade
+from ..youzan import getqr,query_trade,yz_config
+import hashlib
+import json
+import urllib
+
+def VerifySing(data):
+    a=yz_config.client_id+data.get('msg')+yz_config.client_secret
+    hl=hashlib.md5()
+    hl.update(a)
+    hash_=hl.hexdigest()
+    if hash_==data.get('sign'):
+        return True
+    else:
+        return False
+
 
 
 @faka.route('/')
@@ -16,7 +30,7 @@ def index():
 
 @faka.route('/CreateOrder', methods=["POST"])
 def CreateOrder():
-    trade_id = request.form.get('out_trade_no')
+    trade_id = request.form.get('out_trade_no',type=int)
     money = request.form.get('money',type=float)
     if money<0:
         money=1
@@ -45,11 +59,30 @@ def CreateOrder():
 def check_order():
     tradeid = request.args.get('tradeid',type=int)
     order = Order.query.filter_by(trade_id=tradeid).first()
-    trade_status=query_trade(order.qr_id)
-    if trade_status == True:
-        # 订单信息更新
-        order.endtime = datetime.datetime.now()
-        order.trade_status = True
-        db.session.add(order)
-        db.session.commit()
+    if not order.trade_status:
+        trade_status=query_trade(order.qr_id)
+        if trade_status == True:
+            # 订单信息更新
+            order.endtime = datetime.datetime.now()
+            order.trade_status = True
+            db.session.add(order)
+            db.session.commit()
     return redirect(url_for('faka.index'))
+
+
+@faka.route('/order_msg',methods=['POST'])
+def order_msg():
+    data=request.get_json()
+    if not data.get('test'): #是否测试消息
+        if VerifySing(data): #是否伪造
+            if data.get('type')=='trade_TradePaid':#是否交易消息
+                if data.get('status')=='PAID': #如果支付成功则进行操作
+                    msg=json.loads(urllib.unquote(data.get('msg')))
+                    qr_id=msg.get('qr_info').get('qr_id')
+                    order=Order.query.filter_by(qr_id=qr_id).first()
+                    # 订单信息更新
+                    order.endtime = datetime.datetime.now()
+                    order.trade_status = True
+                    db.session.add(order)
+                    db.session.commit()
+    return jsonify({"code":0,"msg":"success"})
